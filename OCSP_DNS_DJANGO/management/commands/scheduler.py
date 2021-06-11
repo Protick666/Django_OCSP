bf140588f0cc2c3499a68c5f7cd65df6844a75dd
protickbhowmick <protickbhowmick@priyo.com>
2021-06-10 23:04:26 -0400

    ocsp re-query function added

--- OCSP_DNS_DJANGO/management/commands/scheduler.py
+++ OCSP_DNS_DJANGO/management/commands/scheduler.py
@@ -274,6 +274,46 @@ def unit_ocsp_url_process(ocsp_url):
             logger.error("Error in Processing cert serial {} for ocsp url {} ({})".format(serial_number, ocsp_url, e))
 
 
+def query_unauthorized_responses():
+    unauthorized_responses = ocsp_data.objects.filter(ocsp_response_status="OCSPResponseStatus.UNAUTHORIZED")
+    total_query_member = unauthorized_responses.count()
+    logger.info("Starting re-querying unauthorized responses, total {}".format(total_query_member))
+
+    flipped_response_count = 0
+    for ocsp_response in unauthorized_responses:
+        try:
+            ocsp_url, serial_number, akid = ocsp_response.ocsp_url.url, ocsp_response.serial, ocsp_response.akid
+
+            ca_cert = fix_cert_indentation(r.get("ocsp:akid:" + akid).decode())
+            ca_cert = pem.readPemFromString(ca_cert)
+            issuerCert, _ = decoder.decode(ca_cert, asn1Spec=rfc2459.Certificate())
+
+            ocsp_host = get_ocsp_host(ocsp_url=ocsp_url)
+            headers = get_ocsp_request_headers(ocsp_host)
+            ocspReq = makeOcspRequest(issuerCert=issuerCert, userSerialNumber=hex(int(serial_number)), userCert=None,
+                                      add_nonce=False)
+
+            import requests as r_req
+            response = r_req.post(url=ocsp_url, data=encoder.encode(ocspReq), headers=headers, timeout=5)
+            decoded_response = return_ocsp_result(response)
+
+            if str(decoded_response.response_status) == "OCSPResponseStatus.SUCCESSFUL":
+                # logger.info("New status: {}".format(str(decoded_response.response_status)))
+                flipped_response_count = flipped_response_count + 1
+
+            ocsp_response.ocsp_response_status = str(decoded_response.response_status)
+            ocsp_response.ocsp_response = response.content
+            if str(decoded_response.response_status) == "OCSPResponseStatus.SUCCESSFUL":
+                if len(decoded_response.certificates) > 0:
+                    # print("delegated")
+                    ocsp_response.delegated_response = True
+
+            ocsp_response.save()
+        except Exception as e:
+            logger.error("Error in re-querying cert serial {} for ocsp url {} ({})".format(serial_number, ocsp_url, e))
+
+    logger.info("Total new successful response: {} out of {}".format(flipped_response_count, total_query_member))
+
 def ocsp_job_mp():
     for i in range(0, 20):
         logger.info("Starting Scheduler Job")

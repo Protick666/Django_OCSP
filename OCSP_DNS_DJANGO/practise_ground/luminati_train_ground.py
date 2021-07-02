c0081f919a40b2719498f900f49d47975faff844
protickbhowmick <protickbhowmick@priyo.com>
2021-07-02 12:15:59 -0400

    cloudflare cache exp added

--- OCSP_DNS_DJANGO/practise_ground/luminati_train_ground.py
+++ OCSP_DNS_DJANGO/practise_ground/luminati_train_ground.py
@@ -1,10 +1,13 @@
+import json
 import logging
 
 import django
+import requests
 
 from OCSP_DNS_DJANGO.management.commands.scheduler import get_ocsp_host, makeOcspRequest, \
-    return_ocsp_result, get_ocsp_request_headers_as_tuples
-
+    return_ocsp_result, get_ocsp_request_headers_as_tuples, get_ocsp_request_headers
+from OCSP_DNS_DJANGO.models import OcspResponsesWrtAsn
+from random import randint
 django.setup()
 
 logger = logging.getLogger(__name__)
@@ -21,7 +24,7 @@ r = redis.Redis(host=redis_host, port=6379, db=0, password="certificatesarealway
 
 import redis
 
-from OCSP_DNS_DJANGO.tools import fix_cert_indentation
+from OCSP_DNS_DJANGO.tools import fix_cert_indentation, get_dns_records
 
 from OCSP_DNS_DJANGO.pyasn1_modules import rfc2459
 from OCSP_DNS_DJANGO.pyasn1_modules import pem
@@ -29,65 +32,131 @@ from pyasn1.codec.der import decoder
 from pyasn1.codec.der import encoder
 
 
-def luminati_master_crawler_debug():
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
 
-    logger.info("Starting ocsp job now !")
+
+def make_ocsp_query(serial_number, akid, r, ocsp_url, ip_host):
+    response = None
+    ca_cert = fix_cert_indentation(r.get("ocsp:akid:" + akid).decode())
+    ca_cert = pem.readPemFromString(ca_cert)
+    issuerCert, _ = decoder.decode(ca_cert, asn1Spec=rfc2459.Certificate())
+    ocsp_host = get_ocsp_host(ocsp_url=ocsp_url)
+    ocspReq = makeOcspRequest(issuerCert=issuerCert, userSerialNumber=hex(int(serial_number)),
+                              userCert=None, add_nonce=False)
+    headers = get_ocsp_request_headers(ocsp_host)
+
+    try:
+        response = requests.post(ip_host, data=encoder.encode(ocspReq), headers=headers)
+        decoded_response = return_ocsp_result(response.content, is_bytes=True)
+        d = {}
+        d['response_status'] = str(decoded_response.response_status)
+        if str(decoded_response.response_status) == "OCSPResponseStatus.SUCCESSFUL":
+            d['cert_status'] = str(decoded_response.certificate_status)
+        d['elapsed_time'] = response.elapsed.total_seconds()
+        return d
+
+    except Exception as e:
+        d = {}
+        d['error'] = e
+        if response:
+            d['elapsed_time'] = response.elapsed.total_seconds()
+        return d
+
+def get_ips_of_urls():
     r = redis.Redis(host=redis_host, port=6379, db=0, password="certificatesarealwaysmisissued")
-    # ocsp_urls_set = r.smembers("ocsp:ocsp_urls")
-    # ocsp_urls_lst = [item.decode() for item in ocsp_urls_set]
-    ocsp_urls_lst = ['http://ocsp.sectigochina.com']
-    # Tune here
+    ocsp_urls_set = r.smembers("ocsp:ocsp_urls")
+    ocsp_urls_lst = [item.decode() for item in ocsp_urls_set]
 
-    logger.info("Processing total {} ocsp urls".format(len(ocsp_urls_lst)))
+    from collections import defaultdict
+    d = defaultdict(lambda: list())
 
-    # 240*214*20*.002333
-    url_count = 0
-    for ocsp_url in ocsp_urls_lst:
-        logger.info("Processed total {} ocsp urls".format(url_count))
-        url_count += 1
+    for url in ocsp_urls_lst:
+        records = get_dns_records(url)
+        a_records = [e[1] for e in records if e[0] == 'A_RECORD']
+        for a_record in a_records:
+            d[url].append(a_record)
 
-        tu = ('81192179725040481647048356377781389991', 'C6545E5A649886C3FBD40F48892B5B2BF3B120AF', 'e93960a575ac256d7f64267fe626fa6dfc7b18dd21792cb7baf9e3efc9673726')
-
-        serial_number, akid, fingerprint = tu[0], tu[1], tu[2]
+    with open("url_to_ips.json", "w") as ouf:
+        json.dump(d, fp=ouf, indent=2)
 
 
-        ca_cert = fix_cert_indentation(r.get("ocsp:akid:" + akid).decode())
-        ca_cert = pem.readPemFromString(ca_cert)
-        issuerCert, _ = decoder.decode(ca_cert, asn1Spec=rfc2459.Certificate())
+def luminati_master_crawler_cloudflare_cache():
+    logger.info("Starting ocsp job now !")
 
-        ocsp_host = get_ocsp_host(ocsp_url=ocsp_url)
+    r = redis.Redis(host=redis_host, port=6379, db=0, password="certificatesarealwaysmisissued")
 
-        ocspReq = makeOcspRequest(issuerCert=issuerCert, userSerialNumber=hex(int(serial_number)),
-                                  userCert=None, add_nonce=False)
+    ocsp_url = 'http://ocsp2.globalsign.com/gsdomainvalsha2g3'
+    ip_host = 'http://104.18.21.226/gsdomainvalsha2g3'
+    OCSP_URL_ID = 110
 
-        asn_list = ['10796', '11776', '133612', '25144', '29571', '35805', '43940']
+    old_list = []
+    new_list = []
+
+    random_list = []
+    master_akid = '3D808279C54882A3C312EEDF990F5735489ED0CB'
+
+    ex_serial = '10028015818766309226464494355'
+
+    old_list = OcspResponsesWrtAsn.objects.filter(ocsp_url_id=OCSP_URL_ID,
+                                                       ocsp_response_status='OCSPResponseStatus.SUCCESSFUL',
+                                                       ocsp_cert_status='OCSPCertStatus.GOOD')[0: 5]
+
+    import random
 
 
-        headers = get_ocsp_request_headers_as_tuples(ocsp_host)
+    q_key = "ocsp:serial:" + ocsp_url
+    elements = r.lrange(q_key, 0, -1)
+    elements = [e.decode() for e in elements]
+    elements = list(set(elements))[0: 100]
+    elements = random.sample(elements, 5)
+    new_list = elements
+
+    for i in range(5):
+        random_list.append(random_with_N_digits(len(ex_serial)))
+
+    from collections import defaultdict
+    ans = defaultdict(lambda: dict())
+    import time
+
+    d = {}
+    for e in old_list:
+        d_d = {}
+        for c in range(3):
+            data = make_ocsp_query(serial_number=e.serial, akid=e.akid, r=r, ocsp_url=ocsp_url, ip_host=ip_host)
+            time.sleep(10)
+            d_d[c] = data
+        d[e.serial] = d_d
+    ans['old'] = d
+
+    d = {}
+    for element in new_list:
+        d_d = {}
+        serial_number, akid, fingerprint = element.split(":")
+        for c in range(3):
+            data = make_ocsp_query(serial_number=serial_number, akid=akid, r=r, ocsp_url=ocsp_url, ip_host=ip_host)
+            time.sleep(10)
+            d_d[c] = data
+        d[serial_number] = d_d
+    ans['new'] = d
+
+    d = {}
+    for e in random_list:
+        d_d = {}
+        for c in range(3):
+            data = make_ocsp_query(serial_number=e, akid=master_akid, r=r, ocsp_url=ocsp_url, ip_host=ip_host)
+            time.sleep(10)
+            d_d[c] = data
+        d[e] = d_d
+    ans['random'] = d
+
+    with open("cache_exp.json", "w") as ouf:
+        json.dump(ans, fp=ouf, indent=2)
 
 
-        for c in asn_list:
-            try:
-                import urllib.request
-                opener = urllib.request.build_opener(
-                    urllib.request.ProxyHandler(
-                        {
-                            'http': 'http://lum-customer-c_9c799542-zone-residential-asn-{}:xm4jk9845cgb@zproxy.lum-superproxy.io:22225'.format(
-                                c)}))
-                opener.addheaders = headers
-                bb = opener.open(ocsp_url, data=encoder.encode(ocspReq))
-                # per_cert_dict = {}
-                # for header in bb.headers.keys():
-                #     if header.startswith('x-luminati'):
-                #         per_cert_dict[header] = bb.headers[header]
 
-                b = bb.read()
-                decoded_response = return_ocsp_result(b, is_bytes=True)
 
-                # print(type(decoded_response))
 
-                if str(decoded_response.response_status) == "OCSPResponseStatus.SUCCESSFUL":
-                    print(decoded_response.certificate_status)
-            except Exception as e:
-                a = 1
 
--- OCSP_DNS_DJANGO/views.py
+++ OCSP_DNS_DJANGO/views.py
@@ -7,10 +7,11 @@ from OCSP_DNS_DJANGO.latency_maker import find_latency
 from OCSP_DNS_DJANGO.luminati_async_asn import luminati_master_crawler_async
 from OCSP_DNS_DJANGO.luminati_async_asn_weird import luminati_master_crawler_debug_now
 from OCSP_DNS_DJANGO.luminati_sanity_checker import check_sanity
-from OCSP_DNS_DJANGO.practise_ground.luminati_train_ground import luminati_master_crawler_debug
+from OCSP_DNS_DJANGO.practise_ground.luminati_train_ground import luminati_master_crawler_cloudflare_cache, \
+    get_ips_of_urls
 
 
 def ocsp_crawler_v2(request):
-    check_certs_for_strings_v2()
+    #luminati_master_crawler_cloudflare_cache()
     return HttpResponse("asdas")
 

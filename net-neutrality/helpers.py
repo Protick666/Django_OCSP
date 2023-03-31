35dfcbb0a484a8c6326d20ab770c953ad4741f0a
protickbhowmick <protickbhowmick@priyo.com>
2023-03-31 00:12:00 -0400

    testing out

--- net-neutrality/helpers.py
+++ net-neutrality/helpers.py
@@ -12,23 +12,48 @@ def get_urls():
 
 
 def get_all_asns():
+    f = open("data/ttl_data_set-live-local-False.json")
+    d = json.load(f)
+
+    asn_set = set()
+    for e in d:
+        asn_set.add(e[0])
+
+    # return list(asn_set)
+
+    asn_to_cn = {}
+    asn_to_addresses = {}
+
     lines = []
     for line in open('data/asns_full_info.json', 'r'):
         lines.append(json.loads(line))
 
-    tot_set = set()
-    filled_set = set()
-    overall_country = set()
-    filled_country = set()
 
     for a in lines:
-        tot_set.add(a['asn'])
-        overall_country.add(a['country']['name'])
-        if a['announcing']['numberPrefixes'] > 0:
-            filled_set.add(a['asn'])
-            filled_country.add(a['country']['name'])
+        asn_to_cn[a['asn']] = a['country']['name']
+        asn_to_addresses[a['asn']] = a['announcing']['numberAddresses']
 
-    return list(tot_set)
+    from collections import defaultdict
+    cn_to_asn_list = defaultdict(lambda : list())
+
+    for asn in asn_set:
+        cn_to_asn_list[asn_to_cn[asn]].append((asn_to_addresses[asn], asn))
+
+    for cn in cn_to_asn_list:
+        cn_to_asn_list[cn].sort(reverse=True)
+
+    ans_arr = []
+    for cn in cn_to_asn_list:
+        if len(cn) == 0:
+            continue
+        if len(cn_to_asn_list[cn]) < 500:
+            ans_arr = ans_arr + cn_to_asn_list[cn]
+        else:
+            ans_arr = ans_arr + cn_to_asn_list[cn][: 500]
+
+    ans_arr = [e[1] for e in ans_arr]
+    return ans_arr
+
 
 def get_korea_asns():
     file1 = open('data/korea', 'r')

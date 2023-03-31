576c47fdab011782fc5cec6449950e3d282889d1
protickbhowmick <protickbhowmick@priyo.com>
2023-03-30 22:00:12 -0400

    testing out

--- net-neutrality/helpers.py
+++ net-neutrality/helpers.py
@@ -1,3 +1,4 @@
+import json
 
 
 def get_urls():
@@ -10,6 +11,25 @@ def get_urls():
     return url_list
 
 
+def get_all_asns():
+    lines = []
+    for line in open('data/asns_full_info.json', 'r'):
+        lines.append(json.loads(line))
+
+    tot_set = set()
+    filled_set = set()
+    overall_country = set()
+    filled_country = set()
+
+    for a in lines:
+        tot_set.add(a['asn'])
+        overall_country.add(a['country']['name'])
+        if a['announcing']['numberPrefixes'] > 0:
+            filled_set.add(a['asn'])
+            filled_country.add(a['country']['name'])
+
+    return list(tot_set)
+
 def get_korea_asns():
     file1 = open('data/korea', 'r')
     Lines = file1.readlines()
@@ -19,3 +39,21 @@ def get_korea_asns():
         asn = segments[0]
         asn_list.append(asn[2:])
     return asn_list
+
+
+def get_korea_asn_to_org():
+    d = {}
+    file1 = open('data/korea', 'r')
+    Lines = file1.readlines()
+    asn_list = []
+    for line in Lines:
+        segments = line.split()
+        asn = segments[0][2:]
+        names = line.split()[1: -1]
+        s = ""
+        for e in names:
+            s = s + e + " "
+        d[asn] = s
+    return d
+
+a = get_korea_asn_to_org()
